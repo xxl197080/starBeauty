e56ef4acac9c32baa9579a4bc2ab50f43653b3cd
zhan1025 <2498627918@qq.com>
2019-07-02 18:03:40 +0800

    '初始化样式'

---
+++ src/views/mine/style.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+import { Icon } from 'antd';
+
+export const Header = styled.header`
+    background: #F8F8F8;
+    height: 1.95rem;
+    position: absolute;
+    z-index: 8;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: auto;
+    border-bottom: 0.05rem solid #d9d9d9;
+    display:flex;
+    justify-content:space-between;
+`
+export const NewIcon = styled(Icon)`
+  font-size: 1rem;
+  padding: 0.4775rem;
+`
